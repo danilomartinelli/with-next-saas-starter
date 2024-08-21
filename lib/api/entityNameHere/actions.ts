3ad73dd6b55c5a668f72263a4a15e61ce42e2a02
Danilo Martinelli <danilomartinelli429@gmail.com>
2024-08-21 12:08:05 -0300

    refactor: :safety_vest: use zod to validate actions

--- lib/api/entityNameHere/actions.ts
+++ lib/api/entityNameHere/actions.ts
@@ -1,5 +1,27 @@
 'use server';
 
-export const someActionName = async (): Promise<null> => {
-  return null;
+import { z } from 'zod';
+
+export interface ISomeActionSchema {
+  param1: string;
+  param2: number;
+}
+
+const getSomeActionSchema = () => {
+  return z.object({
+    param1: z.string(),
+    param2: z.number(),
+  });
+};
+
+export const someActionName = async (
+  data: z.infer<ReturnType<typeof getSomeActionSchema>>,
+) => {
+  const parsedData = await getSomeActionSchema().parseAsync(data);
+  const { param1, param2 } = parsedData;
+
+  return {
+    param1,
+    param2,
+  };
 };
